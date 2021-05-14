6f6af7935dedd36eba88b4b967c1d2c6f543fa60
Carlos Gonzalez <tech.lowz@gmail.com>
2021-05-14 13:03:00 -0500

    improvements to drop_seed() and action algo

--- compete/optimization/spring_challenge_2021.cpp
+++ compete/optimization/spring_challenge_2021.cpp
@@ -30,7 +30,9 @@ class Cell {
         int neigh0, neigh1, neigh2, neigh3, neigh4, neigh5 = -1;
         bool has_tree;
         vector<int> neigh(){
-            return vector<int> {neigh0, neigh1, neigh2, neigh3, neigh4, neigh5};
+            vector<int> neighbors {neigh0, neigh1, neigh2, neigh3, neigh4, neigh5};
+            sort(neighbors.begin(), neighbors.end());
+            return neighbors;
         }
 };
 
@@ -56,25 +58,27 @@ int tree_grow_cost(int tree_size, int size1_trees, int size_2_trees, int size_3_
 }
 
 string drop_seed(Tree tree, vector<Cell> &grid){
-    int richness {0};
     int new_cell_index = -1;
     Cell seed_cell = grid[tree.index];
-    Cell temp_cell {};
     vector<int> neighbor_cells(6);
     for (int i {0}; i < tree.size; i++){
         neighbor_cells = seed_cell.neigh();
-        for (auto cell_index: neighbor_cells){;
+        for (auto cell_index: neighbor_cells){
             if (cell_index == -1)
                 continue;
-            temp_cell = grid[cell_index];
-            if (temp_cell.richness > richness && !temp_cell.has_tree){
-                richness = temp_cell.richness;
-                new_cell_index = cell_index;
+            if (grid[cell_index].has_tree && tree.size - i == 1){
+                continue;
             }
+            new_cell_index = cell_index;
+            break;
         }
         seed_cell = grid[new_cell_index];
     }
+    if (new_cell_index == -1){
+        return "No available cell to seed within reach";
+    } else {
     return "SEED " + to_string(tree.index) + " " + to_string(seed_cell.index);
+    }
 }
 
 int main(){
@@ -82,6 +86,7 @@ int main(){
     cin >> numberOfCells; cin.ignore();
     vector<Cell> grid(numberOfCells);
     vector<Tree> trees(numberOfCells);
+    vector<Tree> my_trees;
     for (int i = 0; i < numberOfCells; i++) {
         cin >> grid[i].index >> grid[i].richness >> grid[i].neigh0 >> grid[i].neigh1 >> grid[i].neigh2 >> grid[i].neigh3 >> grid[i].neigh4 >> grid[i].neigh5; cin.ignore();
     }
@@ -102,12 +107,13 @@ int main(){
         int numberOfTrees; // the current amount of trees
         cin >> numberOfTrees; cin.ignore();
         trees.resize(numberOfTrees);
-        vector<int> my_trees{0,0,0,0,0};
+        vector<int> my_trees_totals{0,0,0,0,0};
         for (int i = 0; i < numberOfTrees; i++) {
             cin >> trees[i].index >> trees[i].size >> trees[i].isMine >> trees[i].isDormant; cin.ignore();
             if (trees[i].isMine){
-                my_trees[trees[i].size] += 1;
-                my_trees[4] += 1;
+                my_trees.push_back(trees[i]);
+                my_trees_totals[trees[i].size] += 1;
+                my_trees_totals[4] += 1;
             }
             grid[trees[i].index].has_tree = true;
         }
@@ -117,42 +123,39 @@ int main(){
             string possibleAction;
             getline(cin, possibleAction); // try printing something from here to start with
         }
-        Tree harvest_tree;
-        int grow_tree {};
-        int tree_points {};
-        int seed_cost {my_trees[0]};
         string action = "WAIT";
-        for (int j{0}; j < trees.size(); j++){
-            if (trees[j].isMine && !trees[j].isDormant){
-                int tree_index = trees[j].index;
-                int tree_size = trees[j].size;
-                int new_tree_points = possible_points(tree_index, nutrients, grid);
-                if (tree_size < 3){
-                    int grow_cost = tree_grow_cost(tree_size, my_trees[1], my_trees[2], my_trees[3]);
-                    if (grow_cost < (sun / 2)){
-                        action = "GROW " + to_string(tree_index);
-                        break;
-                    }
-                }
-                if (new_tree_points < tree_points){
+        for (auto tree: my_trees){
+            if (tree.isDormant){
+                continue;
+            }
+            int grow_cost = tree_grow_cost(tree.size, my_trees_totals[1], my_trees_totals[2], my_trees_totals[3]);
+            int seed_cost = my_trees_totals[0];
+            int complete_cost = 4;
+            if (tree.size == 0 && sun > grow_cost){
+                action = "GROW " + to_string(tree.index);
+                break;
+            } else if (tree.size == 3 && (my_trees_totals[3] > 2 || day > 20) && sun > complete_cost){
+                action = "COMPLETE " + to_string(tree.index);
+                break;
+            } else {
+                if (day < 10 && sun > seed_cost && tree.size != 0){
+                action = drop_seed(tree, grid);
+                if (action == "No available cell to seed within reach"){
+                    action = "WAIT";
                     continue;
                 }
-                harvest_tree = trees[j];
-                tree_points = new_tree_points;
-            }
-        }
-        if (action == "WAIT" && (my_trees[3] > 0)){
-            if (day > 15 && sun >= 4){
-                action = "COMPLETE " + to_string(harvest_tree.index);
-            } else {
-                action = drop_seed(harvest_tree, grid);
-            }
-        }   
+                break;
+                } else if (sun > grow_cost && tree.size < 3){
+                action = "GROW " + to_string(tree.index);
+                break;
+                }
+            }   
+        } 
         // GROW cellIdx | SEED sourceIdx targetIdx | COMPLETE cellIdx | WAIT <message>
         cout << action << endl;
         trees.clear();
-        tree_points = 0;
-        my_trees = {0,0,0,0,0};
+        my_trees.clear();
+        my_trees_totals = {0,0,0,0,0};
 
     }
 }
